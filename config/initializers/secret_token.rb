6b7a8dd66d8aeddbe98ca5d86e0d0890b84f7efa
Maria Pilar Guerra Arias <pili@3scale.net>
2013-07-20 11:07:43 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AddressBookAppDevise::Application.config.secret_key_base = 'cc8393fd052e95bc98d359ede9af7cd35581788a5219df588eb249fc6e29b8fdf2b097f2d0ded7fbdadc95bd509f7e874e1557690127af3058861a450639f4a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AddressBookAppDevise::Application.config.session_store :cookie_store, key: '_address-book-app-devise_session'
