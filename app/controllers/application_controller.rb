3c99d14cc5925239b90ed0f314710b00dd75f8a1
Maria Pilar Guerra Arias <pili@3scale.net>
2013-07-29 17:19:46 +0200

    fix user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require '3scale_client'
 class ApplicationController < ActionController::Base
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
@@ -15,5 +16,12 @@ class ApplicationController < ActionController::Base
       @app_id = params[:client_id]
       $response = $client.report( { :app_id => @app_id, :usage => { method_name => usage_value } } )
       $response.error!('505 Reporting Error', 505) unless $response.success?
+    end
+
+    protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_in) { |u| u.permit( :name, :email ) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit( :name, :email, :password, :password_confirmation ) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit( :name, :email, :password, :password_confirmation, :current_password )}
     end
 end
