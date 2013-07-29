5b8d5cbf05a7c9972b6fce5226285029ea5f4f61
Maria Pilar Guerra Arias <pili@3scale.net>
2013-07-29 15:32:43 +0200

    Fix provider key environment var

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   
-  $client = ThreeScale::Client.new(:provider_key => ENV["MYAPI_PROVIDER_KEY"])
+  $client = ThreeScale::Client.new(:provider_key => ENV['MYAPI_PROVIDER_KEY'])
 
     def threescale_authenticate!
       $response = $client.oauth_authorize(:app_id => params[:client_id])
